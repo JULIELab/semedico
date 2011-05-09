d3758b0a5e3542826df95142143f4c134b6f0e03
faessler <faessler@3005d3a6-6a28-0410-a70f-d0397f31fdf6>
2011-05-09 16:41:18 +0000

    In the term-navigation is now context-sensitive in the sense that it always shows the children of the current path of the selected term. In the beginning, only roots are shown for each facet.
    git-svn-id: https://www.coling.uni-jena.de/svn/semedico/trunk/semedico-frontend@1038 3005d3a6-6a28-0410-a70f-d0397f31fdf6

--- src/main/java/de/julielab/semedico/components/FacetBox.java
+++ src/main/java/de/julielab/semedico/components/FacetBox.java
@@ -1,11 +1,10 @@
 package de.julielab.semedico.components;
 
 import java.text.Format;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-import org.apache.commons.logging.Log;
-import org.apache.solr.util.OpenBitSet;
 import org.apache.tapestry5.Asset;
 import org.apache.tapestry5.ComponentResources;
 import org.apache.tapestry5.Link;
@@ -31,12 +30,10 @@ import de.julielab.semedico.state.IClientIdentificationService;
 import de.julielab.semedico.util.AbbreviationFormatter;
 import de.julielab.semedico.util.DisplayGroup;
 import de.julielab.semedico.util.LabelFilter;
-import de.julielab.stemnet.core.Facet;
 import de.julielab.stemnet.core.FacetConfiguration;
 import de.julielab.stemnet.core.FacetHit;
-import de.julielab.stemnet.core.Label;
 import de.julielab.stemnet.core.FacetTerm;
-import de.julielab.stemnet.search.FacetHitCollectorService;
+import de.julielab.stemnet.core.Label;
 import de.julielab.stemnet.search.IFacetHitCollectorService;
 
 public class FacetBox implements FacetInterface {
@@ -44,45 +41,45 @@ public class FacetBox implements FacetInterface {
 	@Property
 	@Parameter
 	private FacetHit facetHit;
-	
+
 	@Property
 	@Parameter
 	private FacetConfiguration facetConfiguration;
-	
+
 	@SuppressWarnings("unused")
 	@Property
 	@Parameter("false")
 	private boolean showLabelCount;
-	
+
 	@Property
 	@Persist
 	private Format abbreviationFormatter;
-	
+
 	@Property
 	@Persist
 	private DisplayGroup<Label> displayGroup;
-	
+
 	@Property
 	private Label labelItem;
-	
+
 	@SuppressWarnings("unused")
 	@Property
 	private int labelIndex;
-	
+
 	@Parameter
-	private FacetTerm selectedTerm;	
-	
+	private FacetTerm selectedTerm;
+
 	@SuppressWarnings("unused")
 	@Property
 	@Parameter("true")
 	private boolean viewModeSwitchable;
-	
-//	@Parameter
-//	private OpenBitSet documents;
-	
+
+	// @Parameter
+	// private OpenBitSet documents;
+
 	@Property
 	private FacetTerm pathItem;
-	
+
 	@Property
 	private int pathItemIndex;
 
@@ -91,77 +88,111 @@ public class FacetBox implements FacetInterface {
 
 	private static String INIT_JS = "var %s = new FacetBox(\"%s\", \"%s\", %s, %s, %s);";
 
-	@Inject @Path("facetbox.js")
+	@Inject
+	@Path("facetbox.js")
 	private Asset facetBoxJS;
 
 	@Inject
-    private Request request;
+	private Request request;
 
 	@Inject
 	private IFacetHitCollectorService facetHitCollectorService;
-	
+
 	@Inject
 	private Logger logger;
-	
+
 	@Inject
-    private ComponentResources resources;
-	
+	private ComponentResources resources;
+
 	@Environmental
 	private RenderSupport renderSupport;
-	
+
 	@BeginRender
-	public void initialize(){
-		if( abbreviationFormatter == null )
-			abbreviationFormatter = new AbbreviationFormatter(MAX_PATH_ENTRY_LENGTH);
-		if( displayGroup == null ){
+	public void initialize() {
+		if (abbreviationFormatter == null)
+			abbreviationFormatter = new AbbreviationFormatter(
+					MAX_PATH_ENTRY_LENGTH);
+		if (displayGroup == null) {
 			displayGroup = new DisplayGroup<Label>();
 			displayGroup.setBatchSize(3);
 			displayGroup.setFilter(new LabelFilter());
 		}
-		
-		if( facetHit != null ){
+
+		if (facetConfiguration != null
+				&& facetConfiguration.getCurrentPath() != null
+				&& facetConfiguration.getCurrentPath().size() > 0) {
+			int currentBatch = displayGroup.getCurrentBatchNumber();
+			List<Label> labels = new ArrayList<Label>();
+			for (FacetTerm term : facetConfiguration.getCurrentPath()
+					.get(facetConfiguration.getCurrentPath().size() - 1)
+					.getSubTerms()) {
+				Label l = new Label();
+				l.setTerm(term);
+				l.setHits(1);
+				labels.add(l);
+			}
+			for (FacetTerm term : facetConfiguration.getCurrentPath()
+					.get(facetConfiguration.getCurrentPath().size() - 1)
+					.getSubTerms()) {
+				for (FacetTerm child : term.getSubTerms()) {
+					Label l = new Label();
+					l = new Label();
+					l.setTerm(child);
+					l.setHits(1);
+					labels.add(l);
+				}
+			}
+			displayGroup.setAllObjects(labels);
+			displayGroup.displayBatch(currentBatch);
+		} else if (facetHit != null) {
 			int currentBatch = displayGroup.getCurrentBatchNumber();
 			displayGroup.setAllObjects(facetHit.getLabels());
 			displayGroup.displayBatch(currentBatch);
 		}
-	}	
-	
+
+		// if( facetHit != null ){
+		// int currentBatch = displayGroup.getCurrentBatchNumber();
+		// displayGroup.setAllObjects(facetHit.getLabels());
+		// displayGroup.displayBatch(currentBatch);
+		// }
+	}
+
 	@AfterRender
-	void addJavaScript(MarkupWriter markupWriter){
+	void addJavaScript(MarkupWriter markupWriter) {
 		renderSupport.addScriptLink(facetBoxJS);
 		Link link = resources.createEventLink(EVENT_NAME);
 		String id = getClientId();
-		if( id != null )
-			renderSupport.addScript(INIT_JS, id, id, link.toAbsoluteURI(), 
-								facetConfiguration.isExpanded(), 
-								facetConfiguration.isCollapsed(), 
-								facetConfiguration.isHierarchicMode());
-		//renderSupport.addScript("alert('ok');");
-		
+		if (id != null)
+			renderSupport.addScript(INIT_JS, id, id, link.toAbsoluteURI(),
+					facetConfiguration.isExpanded(),
+					facetConfiguration.isCollapsed(),
+					facetConfiguration.isHierarchicMode());
+		// renderSupport.addScript("alert('ok');");
+
 	}
-	
-	public void onTermSelect(int index){
-		if( displayGroup.getDisplayedObjects().size() > index ){
+
+	public void onTermSelect(int index) {
+		if (displayGroup.getDisplayedObjects().size() > index) {
 			Label label = displayGroup.getDisplayedObjects().get(index);
 			selectedTerm = label.getTerm();
-			if( facetConfiguration.isHierarchicMode() ){
+			if (facetConfiguration.isHierarchicMode()) {
 				facetConfiguration.getCurrentPath().clear();
-				facetConfiguration.getCurrentPath().addAll(selectedTerm.getAllParents());
-				if( label.hasChildHits() )
+				facetConfiguration.getCurrentPath().addAll(
+						selectedTerm.getAllParents());
+				if (label.hasChildHits())
 					facetConfiguration.getCurrentPath().add(selectedTerm);
 			}
 		}
-	}	
-	
-	private void changeExpansion(boolean expanded){
-		if( expanded ){
+	}
+
+	private void changeExpansion(boolean expanded) {
+		if (expanded) {
 			displayGroup.displayBatch(1);
 			displayGroup.setBatchSize(20);
-			
+
 			facetConfiguration.setExpanded(true);
-			facetConfiguration.setCollapsed(false);				
-		}
-		else{
+			facetConfiguration.setCollapsed(false);
+		} else {
 			displayGroup.displayBatch(1);
 			displayGroup.setBatchSize(3);
 			facetConfiguration.setExpanded(false);
@@ -172,13 +203,12 @@ public class FacetBox implements FacetInterface {
 		filter.setFilterToken(null);
 		displayGroup.setFilter(filter);
 	}
-	
-	private void changeCollapsation(boolean collapsed){
-		if( collapsed ){
+
+	private void changeCollapsation(boolean collapsed) {
+		if (collapsed) {
 			facetConfiguration.setCollapsed(true);
 			facetConfiguration.setExpanded(false);
-		}
-		else{
+		} else {
 			facetConfiguration.setCollapsed(false);
 			facetConfiguration.setExpanded(false);
 		}
@@ -188,9 +218,9 @@ public class FacetBox implements FacetInterface {
 		displayGroup.setFilter(filter);
 
 		displayGroup.displayBatch(1);
-		displayGroup.setBatchSize(3);			
+		displayGroup.setBatchSize(3);
 	}
-	
+
 	public Object onAction() {
 		String isExpanded = request.getParameter(EXPAND_LIST_PARAM);
 		String collapse = request.getParameter(COLLAPSE_PARAM);
@@ -201,272 +231,286 @@ public class FacetBox implements FacetInterface {
 		String hierarchicMode = request.getParameter(HIERARCHIC_MODE_PARAM);
 		String drillUp = request.getParameter(DRILL_UP_PARAM);
 		String drillToTop = request.getParameter(DRILL_TO_TOP_PARAM);
-		
-		logger.info("trigger() isExpanded: " + isExpanded + " collapse: " + collapse + " pager " + pager + 
-					" filterToken " + filterToken + " clearFilter " + clearFilter + " hide " + hide + 
-					" hierarchicMode " +hierarchicMode + " drillUp " + drillUp + " drillToTop " + drillToTop);
-		
-		if( isExpanded != null )
+
+		logger.info("trigger() isExpanded: " + isExpanded + " collapse: "
+				+ collapse + " pager " + pager + " filterToken " + filterToken
+				+ " clearFilter " + clearFilter + " hide " + hide
+				+ " hierarchicMode " + hierarchicMode + " drillUp " + drillUp
+				+ " drillToTop " + drillToTop);
+
+		if (isExpanded != null)
 			changeExpansion(Boolean.parseBoolean(isExpanded));
-						
-		if( pager != null ){
-			if( pager.equals("next") ){
+
+		if (pager != null) {
+			if (pager.equals("next")) {
 				displayGroup.displayNextBatch();
-			}
-			else{
+			} else {
 				displayGroup.displayPreviousBatch();
-			}					
+			}
 		}
 
-		if( collapse != null )
+		if (collapse != null)
 			changeCollapsation(Boolean.parseBoolean(collapse));
-		
-		if( filterToken != null ){
+
+		if (filterToken != null) {
 			LabelFilter filter = (LabelFilter) displayGroup.getFilter();
 			filter.setFilterToken(filterToken);
 			displayGroup.setFilter(filter);
 		}
 
-		if( clearFilter != null ){
+		if (clearFilter != null) {
 			LabelFilter filter = (LabelFilter) displayGroup.getFilter();
 			filter.setFilterToken(null);
 			displayGroup.setFilter(filter);
 		}
-		
-		if( hide != null && hide.equals("true") ){
+
+		if (hide != null && hide.equals("true")) {
 			facetConfiguration.setHidden(true);
 		}
-		
-		if( hierarchicMode != null ){
+
+		if (hierarchicMode != null) {
 			switchViewMode();
 		}
-		
-		if( drillUp != null ){
+
+		if (drillUp != null) {
 			int index = Integer.parseInt(drillUp);
 			drillUp(index);
 		}
-		
-		if( drillToTop != null ){
+
+		if (drillToTop != null) {
 			drillToTop();
 		}
-		
+
 		return this;
 	}
-	
-	public void drillUp(int index){
+
+	public void drillUp(int index) {
 		List<FacetTerm> path = facetConfiguration.getCurrentPath();
 
-		if( index < 0 || index >= path.size() )
+		if (index < 0 || index >= path.size())
 			return;
-		
-		for( Iterator<FacetTerm> iterator = path.iterator(); iterator.hasNext(); ){
+
+		for (Iterator<FacetTerm> iterator = path.iterator(); iterator.hasNext();) {
 			FacetTerm term = iterator.next();
-			if( path.indexOf(term) > index )
+			if (path.indexOf(term) > index)
 				iterator.remove();
 		}
-		
+
 		refreshFacetHit();
 	}
-	
-	private void refreshFacetHit(){
-		Iterator<FacetHit> hitsIterator = facetHitCollectorService.collectFacetHits(Lists.newArrayList(facetConfiguration)).iterator(); 
 
-		if( hitsIterator.hasNext() ){
+	private void refreshFacetHit() {
+		Iterator<FacetHit> hitsIterator = facetHitCollectorService
+				.collectFacetHits(Lists.newArrayList(facetConfiguration))
+				.iterator();
+
+		if (hitsIterator.hasNext()) {
 			facetHit = hitsIterator.next();
 			displayGroup.setAllObjects(facetHit.getLabels());
 		}
-		
+
 	}
-	
-	public void drillToTop(){
+
+	public void drillToTop() {
 		List<FacetTerm> path = facetConfiguration.getCurrentPath();
 		path.clear();
-		
+
 		refreshFacetHit();
 	}
-	
-	public void switchViewMode(){
 
-		if( facetConfiguration.isHierarchicMode() )
+	public void switchViewMode() {
+
+		if (facetConfiguration.isHierarchicMode())
 			facetConfiguration.getCurrentPath().clear();
-		
-		facetConfiguration.setHierarchicMode(!facetConfiguration.isHierarchicMode());
-	
+
+		facetConfiguration.setHierarchicMode(!facetConfiguration
+				.isHierarchicMode());
+
 		refreshFacetHit();
-		
-	}
-	
-	public String getCollapseLinkId(){
-		return getClientId()+ "CollapseLink";
-	}
-	
-	public String getModeSwitchLinkId(){
-		return getClientId()+ "ModeSwitchLink";
-	}
-	
-	public String getCloseLinkId(){
-		return getClientId()+ "CloseLink";
-	}
-	public String getFacetBoxHeaderPathStyle(){
-		return "display:"+ (facetConfiguration.isCollapsed()? "none": "block;");
-	}
-	
-	public String getPathEntryStyle(){
-		return "margin-left:"+getPathMargin()+"px";
-	}
-	
-	public String getPathLinkId(){
-		return getClientId() + "pathLink" +pathItemIndex;
-	}
-	
-	public String getModeSwitchLinkClass(){
-		return facetConfiguration.isHierarchicMode() ? "modeSwitchLinkList" : "modeSwitchLinkTree"; 
-	}
-	
-	public String getLinkId(){
-		return getClientId()+"Link";
-	}
-	
-	public String getTopLinkId(){
-		return getClientId()+"TopLink";
-	}
-	public String getPagerPreviousLinkId(){
-		return getClientId()+"PagerPreviousLink";
-	}
-	
-	public String getPagerNextLinkId(){
-		return getClientId()+"PagerNextLink";
+
 	}
 
-	public String getLabelFilterStyle(){
+	public String getCollapseLinkId() {
+		return getClientId() + "CollapseLink";
+	}
+
+	public String getModeSwitchLinkId() {
+		return getClientId() + "ModeSwitchLink";
+	}
+
+	public String getCloseLinkId() {
+		return getClientId() + "CloseLink";
+	}
+
+	public String getFacetBoxHeaderPathStyle() {
+		return "display:"
+				+ (facetConfiguration.isCollapsed() ? "none" : "block;");
+	}
+
+	public String getPathEntryStyle() {
+		return "margin-left:" + getPathMargin() + "px";
+	}
+
+	public String getPathLinkId() {
+		return getClientId() + "pathLink" + pathItemIndex;
+	}
+
+	public String getModeSwitchLinkClass() {
+		return facetConfiguration.isHierarchicMode() ? "modeSwitchLinkList"
+				: "modeSwitchLinkTree";
+	}
+
+	public String getLinkId() {
+		return getClientId() + "Link";
+	}
+
+	public String getTopLinkId() {
+		return getClientId() + "TopLink";
+	}
+
+	public String getPagerPreviousLinkId() {
+		return getClientId() + "PagerPreviousLink";
+	}
+
+	public String getPagerNextLinkId() {
+		return getClientId() + "PagerNextLink";
+	}
+
+	public String getLabelFilterStyle() {
 		return "margin-left:" + getElementMargin() + "px;";
 	}
-	
-	public String getLabelStyle(){
+
+	public String getLabelStyle() {
 		return "margin-left:" + getListMargin() + "px;";
 	}
-	
-	public String getBoxFooterLeftStyle(){
-		return "margin-left:"+getFooterMargin()+"px;";
-	}
-	
-	public String getBoxFooterLeftMaximizedStyle(){
+
+	public String getBoxFooterLeftStyle() {
 		return "margin-left:" + getFooterMargin() + "px;";
 	}
-	
-	public String getLabelClass(){
-		if( !facetConfiguration.isHierarchicMode() )
+
+	public String getBoxFooterLeftMaximizedStyle() {
+		return "margin-left:" + getFooterMargin() + "px;";
+	}
+
+	public String getLabelClass() {
+		if (!facetConfiguration.isHierarchicMode())
 			return "list";
-		else if( labelItem.hasChildHits() )
+		else if (labelItem.hasChildHits())
 			return "tree";
 		else
 			return "list";
 	}
-	
-	public boolean showFilter(){
-		 return (facetConfiguration.isExpanded() && 
-				 displayGroup.hasMultipleBatches()) || 
-				 isFiltered();	
+
+	public boolean showFilter() {
+		return (facetConfiguration.isExpanded() && displayGroup
+				.hasMultipleBatches()) || isFiltered();
 	}
-	
-	public boolean showMore(){
+
+	public boolean showMore() {
 		return displayGroup.getAllObjects().size() > 3;
 	}
-	
-	public int getPathMargin(){
-		return (pathItemIndex+1)*7;
+
+	public int getPathMargin() {
+		return (pathItemIndex + 1) * 7;
 	}
-	
-	public int getElementMargin(){		
-		
-		if( client.getName().equals(IClientIdentificationService.IEXPLORER))
-			return getPathMargin()+5;
+
+	public int getElementMargin() {
+
+		if (client.getName().equals(IClientIdentificationService.IEXPLORER))
+			return getPathMargin() + 5;
 		else
-			return getPathMargin()+15;
+			return getPathMargin() + 15;
 	}
-	
-	public int getFooterMargin(){		
-		if( client.equals(Client.IEXPLORER6))
-			return getPathMargin()+5;
+
+	public int getFooterMargin() {
+		if (client.equals(Client.IEXPLORER6))
+			return getPathMargin() + 5;
 		else
-			return getPathMargin()+15;
+			return getPathMargin() + 15;
 	}
-	
-	public boolean isFiltered(){
+
+	public boolean isFiltered() {
 		LabelFilter filter = (LabelFilter) displayGroup.getFilter();
-		return filter.getFilterToken() != null && !filter.getFilterToken().equals("");
+		return filter.getFilterToken() != null
+				&& !filter.getFilterToken().equals("");
 	}
-	
-	public String getFilterValue(){
+
+	public String getFilterValue() {
 		LabelFilter filter = (LabelFilter) displayGroup.getFilter();
-		if( filter.getFilterToken() == null ||  filter.getFilterToken().trim().equals("") )
+		if (filter.getFilterToken() == null
+				|| filter.getFilterToken().trim().equals(""))
 			return "type to filter";
 		else
 			return filter.getFilterToken();
 	}
-	
-	public int getListMargin(){
-		return getPathMargin()+7;
+
+	public int getListMargin() {
+		return getPathMargin() + 7;
 	}
-	
-	public String getPathItemDescription(){
+
+	public String getPathItemDescription() {
 		String description = "";
-		
-		if( pathItem.getShortDescription() != null && !pathItem.getShortDescription().equals("") ){
-			description = "Synonyms: "+ pathItem.getShortDescription()+ "<br/><br/>";
+
+		if (pathItem.getShortDescription() != null
+				&& !pathItem.getShortDescription().equals("")) {
+			description = "Synonyms: " + pathItem.getShortDescription()
+					+ "<br/><br/>";
 			description = description.replace(';', ',');
 		}
 		description += pathItem.getDescription();
-		
-		return description;	
+
+		return description;
 	}
-	
-	public String getLabelDescription(){
+
+	public String getLabelDescription() {
 		String description = "";
-		
+
 		FacetTerm term = labelItem.getTerm();
-		if( term.getShortDescription() != null && !term.getShortDescription().equals("") ){
-			description = "Synonyms: "+ term.getShortDescription()+ "<br/><br/>";
+		if (term.getShortDescription() != null
+				&& !term.getShortDescription().equals("")) {
+			description = "Synonyms: " + term.getShortDescription()
+					+ "<br/><br/>";
 			description = description.replace(';', ',');
 		}
 		description += term.getDescription();
-		
-		return description;	
+
+		return description;
 	}
-	
-	public boolean getIsHidden(){
-		if( facetHit == null )
+
+	public boolean getIsHidden() {
+		if (facetHit == null)
 			return true;
-		if( facetHit.getLabels().size() == 0 )
+		if (facetHit.getLabels().size() == 0)
 			return true;
-		
-		if( facetConfiguration != null && facetConfiguration.isHidden() )
+
+		if (facetConfiguration != null && facetConfiguration.isHidden())
 			return true;
-		
+
 		return false;
 	}
-	
-	public String getClientId(){
-		if( facetHit != null)
+
+	public String getClientId() {
+		if (facetHit != null)
 			return facetHit.getFacet().getCssId();
 		else
 			return null;
 	}
-	
-	public String getBoxId(){
-		return getClientId()+ "Box";
+
+	public String getBoxId() {
+		return getClientId() + "Box";
 	}
 
-	public String getPanelId(){
-		return getClientId()+"Panel";
-	}
-	
-	public String getListId(){
-		return getClientId()+"List";
+	public String getPanelId() {
+		return getClientId() + "Panel";
 	}
 
-	public String getPanelStyle(){
-		return "display:"+ (facetConfiguration.isCollapsed()? "none": "block;");
+	public String getListId() {
+		return getClientId() + "List";
 	}
-		
+
+	public String getPanelStyle() {
+		return "display:"
+				+ (facetConfiguration.isCollapsed() ? "none" : "block;");
+	}
+
 }
