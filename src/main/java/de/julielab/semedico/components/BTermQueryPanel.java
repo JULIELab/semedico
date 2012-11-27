767677ce47f6e7976f265bae3f2d357303d4eefd
faessler <faessler@3005d3a6-6a28-0410-a70f-d0397f31fdf6>
2012-11-27 08:30:24 +0000

    Added information about what the original queries were when reviewing bterm literature.
    git-svn-id: https://www.coling.uni-jena.de/svn/semedico/trunk/semedico-frontend@1401 3005d3a6-6a28-0410-a70f-d0397f31fdf6

--- src/main/java/de/julielab/semedico/components/BTermQueryPanel.java
+++ src/main/java/de/julielab/semedico/components/BTermQueryPanel.java
@@ -1,9 +1,15 @@
 package de.julielab.semedico.components;
 
+import java.util.Collection;
+
 import org.apache.tapestry5.annotations.Parameter;
 import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.annotations.SessionState;
+
+import com.google.common.collect.Multimap;
 
 import de.julielab.semedico.core.Facet;
+import de.julielab.semedico.core.SearchState;
 import de.julielab.semedico.core.taxonomy.interfaces.IFacetTerm;
 
 public class BTermQueryPanel {
@@ -11,6 +17,13 @@ public class BTermQueryPanel {
 	@Parameter
 	@Property
 	private IFacetTerm selectedBTerm;
+	
+	@SessionState
+	@Property
+	private SearchState searchState;
+	
+	@Property
+	private IFacetTerm termLoopItem;
 	
 	public String getBTermCSSClass() {
 		if (selectedBTerm != null) {
@@ -20,5 +33,20 @@ public class BTermQueryPanel {
 			return termClass;
 		} else
 			return null;
+	}
+	
+	public String getSearchNodeTermCSSClass() {
+		if (selectedBTerm != null) {
+			Facet facet = termLoopItem.getFirstFacet();
+			String cssId = facet.getCssId();
+			String termClass = cssId + " filterBox primaryFacetStyle";
+			return termClass;
+		} else
+			return null;
+	}
+	
+	public Collection<IFacetTerm> getSearchNodeTerms(int index) {
+		Multimap<String, IFacetTerm> searchNode = searchState.getSearchNode(index);
+		return searchNode.values();
 	}
 }
