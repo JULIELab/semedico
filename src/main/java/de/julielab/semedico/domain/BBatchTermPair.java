cdb3403f157981a3f050a60c319abe08f91fbf79
faessler <faessler@3005d3a6-6a28-0410-a70f-d0397f31fdf6>
2013-02-11 16:43:08 +0000

    Began to add a page for b term batch analysis.
    git-svn-id: https://www.coling.uni-jena.de/svn/semedico/trunk/semedico-frontend@1413 3005d3a6-6a28-0410-a70f-d0397f31fdf6

---
+++ src/main/java/de/julielab/semedico/domain/BBatchTermPair.java
@@ -0,0 +1,29 @@
+/**
+ * BBatchTermPair.java
+ *
+ * Copyright (c) 2013, JULIE Lab.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Common Public License v1.0
+ *
+ * Author: faessler
+ *
+ * Current version: 1.0
+ * Since version:   1.0
+ *
+ * Creation date: 07.02.2013
+ **/
+
+/**
+ * 
+ */
+package de.julielab.semedico.domain;
+
+/**
+ * @author faessler
+ *
+ */
+public class BBatchTermPair {
+	public String term1;
+	public String term2;
+}
+
