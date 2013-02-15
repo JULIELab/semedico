a647e6ceb529410ec14ec6f3edfee4273337a607
faessler <faessler@3005d3a6-6a28-0410-a70f-d0397f31fdf6>
2013-02-15 13:56:33 +0000

    Now, multiple term pairs can be specified for all of which a b term analysis is performed. Results may be saved via hibernate. Not tested on large data yet.
    git-svn-id: https://www.coling.uni-jena.de/svn/semedico/trunk/semedico-frontend@1414 3005d3a6-6a28-0410-a70f-d0397f31fdf6

--- src/main/java/de/julielab/semedico/pages/BBatchAnalysis.java
+++ src/main/java/de/julielab/semedico/pages/BBatchAnalysis.java
@@ -18,33 +18,44 @@
  */
 package de.julielab.semedico.pages;
 
+import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import java.util.Stack;
+import java.util.Locale;
 
+import org.apache.tapestry5.ComponentResources;
 import org.apache.tapestry5.FieldTranslator;
+import org.apache.tapestry5.Link;
 import org.apache.tapestry5.MarkupWriter;
 import org.apache.tapestry5.ValidationException;
+import org.apache.tapestry5.annotations.Environmental;
+import org.apache.tapestry5.annotations.Import;
 import org.apache.tapestry5.annotations.InjectComponent;
 import org.apache.tapestry5.annotations.Persist;
 import org.apache.tapestry5.annotations.Property;
 import org.apache.tapestry5.corelib.components.Form;
 import org.apache.tapestry5.corelib.components.Zone;
 import org.apache.tapestry5.ioc.annotations.Inject;
+import org.apache.tapestry5.services.javascript.JavaScriptSupport;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.slf4j.Logger;
 
 import com.google.common.collect.HashMultimap;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Multimap;
+import com.google.common.collect.TreeMultiset;
 
 import de.julielab.semedico.bterms.interfaces.IBTermService;
 import de.julielab.semedico.core.Label;
 import de.julielab.semedico.core.TermLabel;
 import de.julielab.semedico.core.exceptions.EmptySearchComplementException;
 import de.julielab.semedico.core.exceptions.TooFewSearchNodesException;
+import de.julielab.semedico.core.services.interfaces.ITermService;
 import de.julielab.semedico.core.taxonomy.interfaces.IFacetTerm;
 import de.julielab.semedico.domain.BBatchTermPair;
+import de.julielab.semedico.entities.BBatchEntity;
 import de.julielab.semedico.query.IQueryDisambiguationService;
 import de.julielab.semedico.query.TermAndPositionWrapper;
 import de.julielab.util.DisplayGroup;
@@ -54,31 +65,74 @@ import de.julielab.util.LabelFilter;
  * @author faessler
  * 
  */
+
+@Import(library = { "bbatchanalysis.js", "context:js/jquery-1.7.1.min.js" }, stylesheet = { "context:css/bbatchanalysis.css" })
 public class BBatchAnalysis {
 
-	@Persist
-	@Property
-	private Stack<BBatchTermPair> termPairs;
+	@Inject
+	Session hibernateSession;
 
+	// @Property
+	// @Persist
+	// private Stack<BBatchTermPair> termPairs;
+
+	@SuppressWarnings("unused")
 	@Property
 	private BBatchTermPair loopItemPair;
 
 	@Property
 	private int termPairIndex;
 
-	@Persist
 	@Property
-	private int numPairs;
+	@Persist
+	private BBatchEntity bBatchEntity;
+
+	// @Property
+	// @Persist
+	// private int numPairs;
+	//
+	// @Property
+	// @Persist
+	// private int numTopTerms;
 
 	@Property
 	@Persist
-	private int numTopTerms;
+	private DisplayGroup<Label> topBTermLabels;
+
+	// @Property
+	// @Persist
+	// private Stack<Integer> bTermFindingNumbers;
+
+	// @Property
+	// @Persist
+	// private boolean filterNonTerms;
 
 	@InjectComponent
 	private Form termform;
 
 	@InjectComponent
 	private Zone termPairsZone;
+
+	@InjectComponent
+	private Zone textAreaZone;
+
+	@InjectComponent
+	private Form saveform;
+
+	@InjectComponent
+	private Form newAnalysisForm;
+
+	@InjectComponent
+	private Form loadform;
+
+	@Property
+	private Long loadResultId;
+
+	@Environmental
+	private JavaScriptSupport javaScriptSupport;
+
+	@Inject
+	private ComponentResources resources;
 
 	@Inject
 	private IQueryDisambiguationService queryDisambiguationService;
@@ -87,41 +141,74 @@ public class BBatchAnalysis {
 	private IBTermService btermService;
 
 	@Inject
+	private ITermService termService;
+
+	// @Persist
+	// private Stack<List<Label>> bTermLists;
+
+	@Persist
+	private NumberFormat f;
+
+	@Inject
 	Logger log;
 
-	@Property
-	@Persist
-	private DisplayGroup<Label> topBTermLabels;
+	private FieldTranslator<DisplayGroup> labelTranslatorForTextArea = new FieldTranslator<DisplayGroup>() {
 
-	@Property
-	@Persist
-	private Stack<Integer> bTermFindingNumbers;
+		@Override
+		public Class<DisplayGroup> getType() {
+			return DisplayGroup.class;
+		}
 
-	@Persist
-	private Stack<List<Label>> bTermLists;
+		@Override
+		public DisplayGroup<Label> parse(String input)
+				throws ValidationException {
+			// output only
+			return null;
+		}
 
-	@Property
-	private boolean filterWords;
+		@Override
+		public String toClient(DisplayGroup value) {
+			StringBuilder sb = new StringBuilder();
+			for (Object o : value.getAllObjects()) {
+				Label l = (Label) o;
+				sb.append("(");
+				sb.append(f.format(l.getRankScore()));
+				sb.append(") ");
+				sb.append(l.getName());
+				sb.append("\n");
+			}
+			return sb.toString();
+		}
+
+		@Override
+		public void render(MarkupWriter writer) {
+			// TODO Auto-generated method stub
+
+		}
+
+	};
 
 	public void setupRender() {
-		if (null == termPairs) {
-			termPairs = new Stack<BBatchTermPair>();
-			numPairs = 2;
+		if (null == bBatchEntity)
+			bBatchEntity = new BBatchEntity();
+		if (0 == bBatchEntity.numTopTerms)
+			bBatchEntity.numTopTerms = 20;
+		if (null == topBTermLabels)
+			topBTermLabels = new DisplayGroup<Label>(new LabelFilter(),
+					bBatchEntity.numPairs * bBatchEntity.numTopTerms,
+					TreeMultiset.<Label> create());
+		if (null == f) {
+			f = NumberFormat.getNumberInstance(Locale.US);
+			f.setMaximumFractionDigits(2);
+			f.setMinimumFractionDigits(2);
 		}
-		if (0 == numTopTerms)
-			numTopTerms = 20;
-		if (null == bTermFindingNumbers)
-			bTermFindingNumbers = new Stack<Integer>();
-		if (null == bTermLists)
-			bTermLists = new Stack<List<Label>>();
-		adjustTermPairs();
+		bBatchEntity.adjustTermPairs();
 	}
 
-	public void onPrepareForRender() {
-	}
+	// ------------------------------ Main input form ------------------------
 
 	public void onValidateFromTermform() {
-		for (BBatchTermPair b : termPairs) {
+		for (BBatchTermPair b : bBatchEntity.termPairs) {
 			if (null == b.term1 || null == b.term2) {
 				termform.recordError("Please specify both terms for each term pair.");
 				return;
@@ -129,7 +216,7 @@ public class BBatchAnalysis {
 		}
 		List<List<Multimap<String, IFacetTerm>>> parsedTerms = new ArrayList<List<Multimap<String, IFacetTerm>>>();
 		int i = 1;
-		for (BBatchTermPair b : termPairs) {
+		for (BBatchTermPair b : bBatchEntity.termPairs) {
 			Multimap<String, TermAndPositionWrapper> parsed1 = queryDisambiguationService
 					.disambiguateQuery(b.term1, null);
 			Multimap<String, IFacetTerm> parsed1Old = getOldQueryStructure(parsed1);
@@ -142,16 +229,17 @@ public class BBatchAnalysis {
 		}
 
 		i = 1;
-		bTermFindingNumbers.clear();
-		topBTermLabels = new DisplayGroup<Label>(new LabelFilter(), numPairs * numTopTerms);
+		bBatchEntity.bTermFindingNumbers.clear();
+		topBTermLabels = new DisplayGroup<Label>(new LabelFilter(),
+				bBatchEntity.numPairs * bBatchEntity.numTopTerms,
+				TreeMultiset.<Label> create());
 		for (List<Multimap<String, IFacetTerm>> pair : parsedTerms) {
 			try {
 				List<Label> bTermLabelList = btermService
 						.determineBTermLabelList(pair);
-				bTermLists.add(bTermLabelList);
-				bTermFindingNumbers.add(bTermLabelList.size());
+				bBatchEntity.bTermLists.add(bTermLabelList);
+				bBatchEntity.bTermFindingNumbers.add(bTermLabelList.size());
 				log.debug("Retrieved {} b-terms", bTermLabelList.size());
-				getTopTerms(bTermLabelList);
 			} catch (TooFewSearchNodesException e) {
 				e.printStackTrace();
 			} catch (EmptySearchComplementException e) {
@@ -165,29 +253,54 @@ public class BBatchAnalysis {
 		log.debug("obtained {} top-b-terms", topBTermLabels.size());
 	}
 
+	public void onSuccessFromTermform() {
+		refreshTopTerms();
+	}
+
+	// Called by the select's zone parameter
+	Object onValueChanged(int value) {
+		bBatchEntity.numPairs = value;
+		bBatchEntity.adjustTermPairs();
+		return termPairsZone.getBody();
+	}
+
+	// Called from custom JavaScript Ajax call since the CheckBox component
+	// doesn't have a zone parameter.
+	Object onCheckboxClicked(boolean state) {
+		bBatchEntity.filterNonTerms = state;
+		refreshTopTerms();
+		return textAreaZone.getBody();
+	}
+
+	private void refreshTopTerms() {
+		topBTermLabels.clear();
+		for (List<Label> bTermLabelList : bBatchEntity.bTermLists)
+			getTopTermsFromList(bTermLabelList);
+	}
+
 	/**
 	 * Fills {@link #topBTermLabels} with all labels which are in the top-
 	 * {@link #numTopTerms} after filtering is applied.
 	 * 
 	 * @param bTermLabelList
 	 */
-	private void getTopTerms(List<Label> bTermLabelList) {
-		for (int j = 0; j < numTopTerms && j < bTermLabelList.size(); j++) {
+	private void getTopTermsFromList(List<Label> bTermLabelList) {
+		int currentNumTopTerms = 0;
+		for (int j = 0; currentNumTopTerms < bBatchEntity.numTopTerms
+				&& j < bTermLabelList.size(); j++) {
 			Label label = bTermLabelList.get(j);
-			if (filterWords) {
+			if (bBatchEntity.filterNonTerms) {
 				if (!(label instanceof TermLabel)) {
-					j--;
 					continue;
 				}
 			}
 			topBTermLabels.add(label);
+			currentNumTopTerms++;
 		}
 	}
 
-	public void onSuccess() {
-
-	}
-
+	// only required because the current representation of disambiguated queries
+	// is stuck between versions. Should all be replaced by the ParseTree
 	private Multimap<String, IFacetTerm> getOldQueryStructure(
 			Multimap<String, TermAndPositionWrapper> result) {
 		// TODO this is for legacy reasons until the new query structure can be
@@ -205,6 +318,8 @@ public class BBatchAnalysis {
 		return disambiguatedQuery;
 	}
 
+	// should be a method of the query object; this will have to wait until we
+	// HAVE a decent query structure, namely the ParseTree
 	private String getQueryString(Multimap<String, IFacetTerm> parsedQuery) {
 		String ret = "\n";
 		for (String key : parsedQuery.keySet()) {
@@ -217,65 +332,88 @@ public class BBatchAnalysis {
 		return ret;
 	}
 
-	public Object onValueChanged(int value) {
-		numPairs = value;
-		adjustTermPairs();
-		return termPairsZone.getBody();
+	@SuppressWarnings("rawtypes")
+	public FieldTranslator<DisplayGroup> getFieldTranslatorForTextArea() {
+		return labelTranslatorForTextArea;
 	}
 
-	/**
-	 * 
-	 */
-	private void adjustTermPairs() {
-		if (numPairs == termPairs.size())
-			return;
-		while (numPairs > termPairs.size()) {
-			termPairs.add(new BBatchTermPair());
-			bTermFindingNumbers.add(0);
-		}
-		while (numPairs < termPairs.size()) {
-			termPairs.pop();
-			bTermFindingNumbers.pop();
-		}
-	}
-
-	public FieldTranslator<ArrayList> getFieldTranslator() {
-		FieldTranslator<ArrayList> ft = new FieldTranslator<ArrayList>() {
-
-			@Override
-			public Class<ArrayList> getType() {
-				return ArrayList.class;
-			}
-
-			@Override
-			public ArrayList<Label> parse(String input)
-					throws ValidationException {
-				// TODO Auto-generated method stub
-				return null;
-			}
-
-			@Override
-			public String toClient(ArrayList value) {
-				StringBuilder sb = new StringBuilder();
-				for (Object o : value) {
-					sb.append(((Label) o).getName());
-					sb.append("\n");
-				}
-				return sb.toString();
-			}
-
-			@Override
-			public void render(MarkupWriter writer) {
-				// TODO Auto-generated method stub
-
-			}
-
-		};
-		return ft;
-	}
-
+	// ---------------------- Property Methods -------------------------------
 	public int getBTermNumber() {
-		return bTermFindingNumbers.get(termPairIndex);
+		if (termPairIndex < bBatchEntity.bTermFindingNumbers.size())
+			return bBatchEntity.bTermFindingNumbers.get(termPairIndex);
+		return 0;
+	}
+
+	public String getTextAreaContent() {
+		if (null != topBTermLabels) {
+			String ret = labelTranslatorForTextArea.toClient(topBTermLabels);
+			return ret;
+		}
+		return null;
+	}
+
+	// ---------------------- Property Methods End ---------------------------
+
+	// ------------------------------ Save Result form -----------------------
+
+	void onValidateFromSaveform() {
+		if (0 == topBTermLabels.size()) {
+			saveform.recordError("There is currently no result to store.");
+		}
+	}
+
+	void onSuccessFromSaveform() {
+		Transaction t = hibernateSession.beginTransaction();
+		if (null == bBatchEntity.id) {
+			hibernateSession.persist(bBatchEntity);
+		} else {
+			hibernateSession.update(bBatchEntity);
+		}
+		t.commit();
+	}
+
+	public String getSaveformLabel() {
+		return bBatchEntity.id == null ? "Save this result"
+				: "Update this result";
+	}
+
+	// ------------------------------ New Analysis form ----------------------
+
+	void onSuccessFromNewAnalysisForm() {
+		resetAllForms();
+	}
+
+	// ------------------------------ Load Result form -----------------------
+
+	void onValidateFromLoadform() {
+		if (null == loadResultId) {
+			loadform.recordError("You must specify the ID of a stored result.");
+		}
+	}
+
+	void onSuccessFromLoadform() {
+		Transaction t = hibernateSession.beginTransaction();
+		BBatchEntity loaded = (BBatchEntity) hibernateSession.get(
+				BBatchEntity.class, loadResultId);
+		t.commit();
+		if (null == loaded)
+			loadform.recordError("The result with ID " + loadResultId
+					+ " could not be found.");
+		else {
+			bBatchEntity = loaded;
+			bBatchEntity.recoverFromDeserialization(termService);
+			refreshTopTerms();
+		}
+	}
+
+	private void resetAllForms() {
+		bBatchEntity = null;
+		topBTermLabels = null;
+	}
+
+	void afterRender() {
+		Link link = resources.createEventLink("checkboxClicked");
+		javaScriptSupport.addScript("filterNonTermCheckbox('%s')", link);
 	}
 
 }
