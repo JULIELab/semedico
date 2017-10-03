f06de94b71d0a215bd1e42e9ab6152db50655d74
khituras <chew@gmx.net>
2017-10-03 16:13:30 +0200

    Added the index option.

--- semedico-es-index-setup/src/main/java/de/julielab/semedico/elasticsearch/index/setup/property/TextSpanProperty.java
+++ semedico-es-index-setup/src/main/java/de/julielab/semedico/elasticsearch/index/setup/property/TextSpanProperty.java
@@ -7,7 +7,7 @@ import de.julielab.semedico.elasticsearch.index.setup.MappingTypes;
 public class TextSpanProperty extends MappingProperty {
 
 	public TextSpanProperty() {
-		super(MappingTypes.preanalyzed, false, AnalyzerTypes.semedico_text, TermVector.with_positions_offsets);
+		super(MappingTypes.preanalyzed, false, true, AnalyzerTypes.semedico_text, TermVector.with_positions_offsets);
 	}
 
 }
