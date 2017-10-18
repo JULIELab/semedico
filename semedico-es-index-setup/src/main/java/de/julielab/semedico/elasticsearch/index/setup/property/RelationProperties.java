d92cb2f1c8793dc17415849c4f8df1163290f2e7
khituras <chew@gmx.net>
2017-10-18 19:03:47 +0200

    Elaborated relations properties. Enhanced creation script with prefix.

--- semedico-es-index-setup/src/main/java/de/julielab/semedico/elasticsearch/index/setup/property/RelationProperties.java
+++ semedico-es-index-setup/src/main/java/de/julielab/semedico/elasticsearch/index/setup/property/RelationProperties.java
@@ -8,14 +8,30 @@ import de.julielab.semedico.elasticsearch.index.setup.MappingTypes;
 
 public class RelationProperties extends MappingProperties {
 	public MappingProperty arguments;
+	public MappingProperty argumentwords;
 	public MappingProperty types;
 	public MappingProperty likelihood;
 	public MappingProperty sentence;
+	public MappingProperty begin;
+	public MappingProperty end;
+	public MappingProperty sentencebegin;
+	public MappingProperty sentenceend;
+	public MappingProperty numarguments;
+	public MappingProperty numdistinctarguments;
+	public MappingProperty source;
 	
 	public RelationProperties() {
-		arguments = new MappingProperty(MappingTypes.keyword, true);
-		types = new MappingProperty(MappingTypes.keyword, true);
-		likelihood = new MappingProperty(MappingTypes.integer, true);
+		arguments = new MappingProperty(MappingTypes.keyword, false, true);
+		argumentwords = new MappingProperty(MappingTypes.text, false, true);
+		types = new MappingProperty(MappingTypes.keyword, false, true);
+		likelihood = new MappingProperty(MappingTypes.integer, true, true);
 		sentence = new MappingProperty(MappingTypes.preanalyzed, false, true, AnalyzerTypes.semedico_text, TermVector.with_positions_offsets, false);
+		begin = new MappingProperty(MappingTypes.integer, true);
+		end = new MappingProperty(MappingTypes.integer, true);
+		sentencebegin = new MappingProperty(MappingTypes.integer, true);
+		sentenceend = new MappingProperty(MappingTypes.integer, true);
+		numarguments = new MappingProperty(MappingTypes.integer, false, true);
+		numdistinctarguments = new MappingProperty(MappingTypes.integer, false, true);
+		source = new MappingProperty(MappingTypes.text, true, true);
 	}
 }
