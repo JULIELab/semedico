3c0043ac53a0ab2c2b573abd254c04fc092e886b
Erik Faessler <khituras@gmail.com>
2018-09-20 14:53:36 +0200

    Adjusting scripts to runnable outside of the faessler account

--- semedico-indexing-pipeline/runslurm.sh
+++ semedico-indexing-pipeline/runslurm.sh
@@ -3,5 +3,5 @@
 #SBATCH --cpus-per-task 1 
 #SBATCH -J semedico_processing 
 
-srun java -jar -Xmx5g -Dlogback.configurationFile=/data/semedico/fushin-2.2/semedico-indexing-pipeline/config/logback.xml ~/bin/jcore-pipeline-runner-*.jar pipelinerunner.xml
+srun java -jar -Xmx5g -Dlogback.configurationFile=/data/semedico/fushin-2.2/semedico-indexing-pipeline/config/logback.xml /var/data/semedico/jcore-pipeline-runner-*.jar pipelinerunner.xml
 
--- semedico-preprocessing-pipeline/runslurm.sh
+++ semedico-preprocessing-pipeline/runslurm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --mem 80g
+#SBATCH --mem 70g
 #SBATCH --cpus-per-task 20 
 #SBATCH -J semedico_processing 
 
-srun java -jar -Xmx75g -Dlogback.configurationFile=/home/faessler/Coding/git/semedico/semedico-preprocessing-pipeline/config/logback.xml ~/bin/jcore-pipeline-runner-*.jar pipelinerunner.xml
+srun java -jar -Xmx60g -Dlogback.configurationFile=/home/faessler/Coding/git/semedico/semedico-preprocessing-pipeline/config/logback.xml /var/data/semedico/jcore-pipeline-runner-*.jar pipelinerunner.xml
 
