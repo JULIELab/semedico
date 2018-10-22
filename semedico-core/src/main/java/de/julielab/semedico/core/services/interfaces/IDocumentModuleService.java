3eb3f77fa2822a53035f3d671c8ae0c9c8c7fac1
khituras <chew@gmx.net>
2018-10-22 10:00:27 +0200

    Starting to implement the base services for document modules.

--- semedico-core/src/main/java/de/julielab/semedico/core/services/interfaces/IDocumentModuleService.java
+++
@@ -1,9 +0,0 @@
-package de.julielab.semedico.core.services.interfaces;
-
-import de.julielab.semedico.docmods.base.services.documentmodules.DocumentModuleInfo;
-
-import java.util.List;
-
-public interface IDocumentModuleService {
-    List<DocumentModuleInfo> getDocumentModuleInfo();
-}
