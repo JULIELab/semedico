38b1e2c46f598b4ec954067862c679c60560e70f
khituras <chew@gmx.net>
2019-06-16 15:54:33 +0200

    NOW actually added the ElasticsearchQueryComponent. Git hickup.

---
+++ semedico-core/src/main/java/de/julielab/scicopia/core/search/ElasticsearchQueryComponent.java
@@ -0,0 +1,63 @@
+package de.julielab.scicopia.core.search;
+
+
+import de.julielab.elastic.query.components.AbstractSearchComponent;
+import de.julielab.elastic.query.components.data.ElasticSearchCarrier;
+import de.julielab.elastic.query.components.data.SearchCarrier;
+import de.julielab.scicopia.core.elasticsearch.IElasticsearchQueryBuilder;
+import de.julielab.semedico.core.search.components.data.SemedicoESSearchCarrier;
+import de.julielab.semedico.core.search.query.QueryToken;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.slf4j.Logger;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ElasticsearchQueryComponent extends AbstractSearchComponent<SemedicoESSearchCarrier> {
+	
+	@Retention(RetentionPolicy.RUNTIME)
+	public @interface ElasticsearchQuery {
+		//
+	}
+	
+	private IElasticsearchQueryBuilder elasticsearchQueryService;
+
+	public ElasticsearchQueryComponent(Logger log, IElasticsearchQueryBuilder elasticsearchQueryService) {
+		super(log);
+		this.elasticsearchQueryService = elasticsearchQueryService;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see de.julielab.semedico.search.components.ISearchComponent#process(de.julielab
+	 * .semedico.search.components.SearchCarrier)
+	 */
+	@Override
+	public boolean processSearch(SemedicoESSearchCarrier semCarrier) {
+		// This code has to be changed to:
+		// * Not be a search component but a part of the QueryAnalysisComponent
+		// * Create an IElasticQuery
+
+//		List<QueryToken> tokens = semCarrier.getUserQuery();
+//
+//		if (null == tokens) {
+//			throw new IllegalArgumentException("A list of " + QueryToken.class.getName()
+//					+ " is expected, but it was null.");
+//		}
+//
+//		if (tokens.isEmpty()) {
+//			throw new IllegalArgumentException("The passed list of " + QueryToken.class.getName()
+//					+ " is invalid. The user query is empty.");
+//		}
+//
+//		SearchServerCommand serverCmds = semCarrier.getSingleSearchServerCommandOrCreate();
+//		serverCmds.query = elasticsearchQueryService.analyseQueryString(tokens);
+//		Map<String, QueryBuilder> namedQueries = new HashMap<>();
+//		serverCmds.namedQueries = namedQueries;
+		return false;
+	}
+}
