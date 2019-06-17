550983c8d7fa72aef906c4a7691157fcc3ec6b43
khituras <chew@gmx.net>
2019-06-17 17:39:30 +0200

    Added a test method to `ElasticsearchQueryBuilderTest`.
    However, I am not sure if the outcome of the code is the desired or not.
    Despite the fact that there is a conjunction, one respective
    query part is wrapped into a "should" clause which doesn't seem right.

--- semedico-core/src/test/java/de/julielab/scicopia/core/elasticsearch/ElasticsearchQueryBuilderTest.java
+++ semedico-core/src/test/java/de/julielab/scicopia/core/elasticsearch/ElasticsearchQueryBuilderTest.java
@@ -4,8 +4,12 @@ import static org.junit.Assert.*;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import de.julielab.semedico.core.TestUtils;
+import de.julielab.semedico.core.services.interfaces.ITokenInputService;
+import org.apache.tapestry5.ioc.Registry;
 import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.TermQueryBuilder;
@@ -72,6 +76,36 @@ public class ElasticsearchQueryBuilderTest {
 		tokens.add(token);
 		BoolQueryBuilder query = (BoolQueryBuilder) builder.analyseQueryString(tokens);
 		assertEquals(2, query.must().size());
+	}
+
+	@Test
+	public void testListener() {
+		final Registry testRegistry = TestUtils.createTestRegistry();
+		final IElasticsearchQueryBuilder qb = testRegistry.getService(IElasticsearchQueryBuilder.class);
+		List<QueryToken> tokens = new ArrayList<>();
+		final QueryToken qt1 = new QueryToken(0, 6, "bonsai");
+		qt1.setType(QueryToken.Category.ALPHA);
+		qt1.setInputTokenType(ITokenInputService.TokenType.KEYWORD);
+		final QueryToken qt2 = new QueryToken(7, 10, "and");
+		qt2.setType(QueryToken.Category.AND);
+		qt2.setInputTokenType(ITokenInputService.TokenType.AND);
+		final QueryToken qt3 = new QueryToken(11, 12, "(");
+		qt3.setType(QueryToken.Category.LPAR);
+		qt3.setInputTokenType(ITokenInputService.TokenType.LEFT_PARENTHESIS);
+		final QueryToken qt4 = new QueryToken(12, 17, "tulip");
+		qt4.setType(QueryToken.Category.ALPHA);
+		qt4.setInputTokenType(ITokenInputService.TokenType.KEYWORD);
+		final QueryToken qt5 = new QueryToken(18, 21, "or");
+		qt5.setType(QueryToken.Category.OR);
+		qt5.setInputTokenType(ITokenInputService.TokenType.OR);
+		final QueryToken qt6 = new QueryToken(22, 26, "rose");
+		qt6.setType(QueryToken.Category.ALPHA);
+		qt6.setInputTokenType(ITokenInputService.TokenType.KEYWORD);
+		final QueryToken qt7 = new QueryToken(26, 27, ")");
+		qt7.setType(QueryToken.Category.RPAR);
+		qt7.setInputTokenType(ITokenInputService.TokenType.RIGHT_PARENTHESIS);
+		final QueryBuilder queryBuilder = qb.analyseQueryString(new ArrayList<>(Arrays.asList(qt1, qt2, qt3, qt4, qt5, qt6, qt7)));
+		System.out.println(queryBuilder.toString());
 	}
 
 }
